d5b5c5d7c5c73b3837a395db5eabbd43228dd30f
Alessandro Giannotta <aleGiannotta@github.com>
2022-01-02 15:49:33 +0100

    new

--- basicCode/helloWorld.cpp
+++ basicCode/helloWorld.cpp
@@ -1,10 +1,11 @@
-	// A hello world program in C++
+// A hello world program in C++
 
-	#include<iostream>
-	using namespace std;
+#include<iostream>
+using namespace std;
 
-	int main()
+int main()
 	{
 		cout << "Hello World!";
+		cout << 1+2 << endl;
 		return 0;
 	}
